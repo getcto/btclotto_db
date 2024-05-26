7bcffda10f086709bf169ab6589ed28f8c28ceeb
jaminpie266 <jamjam@jaminpie.me>
2024-05-26 18:53:27 +0800

    update auth service twitter returb

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   async validateUser(profile: any): Promise<any> {
-    const { username, displayName, profilePicture } = profile;
+    const { username, displayName, profile_image_url } = profile;
     // Here you would typically find or create the user in your database
     // For demonstration, we return a mock user object
 
     const user = {
       username,
       displayName,
-      profilePicture,
+      profile_image_url,
     };
 
     return user;
