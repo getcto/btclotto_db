41a5aae44d7b5005898f469ede083560173ddb4c
jaminpie266 <jamjam@jaminpie.me>
2024-05-28 02:18:03 +0800

    update redirect url

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -44,7 +44,7 @@ export class AuthController {
     const user = req.user;
     res.json(user);
     // Optionally, redirect to your frontend application
-    // res.redirect('/');
+    res.redirect('https://test.dragoorbz.com/profile');
   }
 
   
